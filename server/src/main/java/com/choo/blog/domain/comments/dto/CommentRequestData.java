3904af4bde80a8317e4925a83fd7e6ef627401f3
dddgg9511 <ddgg9511@naver.com>
2022-05-07 14:43:51 +0900

    test : comments save test 코드 작성

--- server/src/main/java/com/choo/blog/domain/comments/dto/CommentRequestData.java
+++ server/src/main/java/com/choo/blog/domain/comments/dto/CommentRequestData.java
@@ -1,4 +1,25 @@
 package com.choo.blog.domain.comments.dto;
 
+import com.choo.blog.domain.comments.Comments;
+import com.choo.blog.domain.users.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class CommentRequestData {
+    private String content;
+    private boolean secret;
+
+    public Comments createEntity(User author, Long postId){
+        return Comments.builder()
+                .content(content)
+                .secret(secret)
+                .author(author)
+                .build();
+    }
 }
--- server/src/test/java/com/choo/blog/domain/comments/service/CommentServiceTest.java
+++ server/src/test/java/com/choo/blog/domain/comments/service/CommentServiceTest.java
@@ -6,6 +6,8 @@ import com.choo.blog.domain.categories.Category;
 import com.choo.blog.domain.categories.dto.CategoryRequestData;
 import com.choo.blog.domain.categories.service.CategoryService;
 import com.choo.blog.domain.categories.service.CategoryServiceTest;
+import com.choo.blog.domain.comments.Comments;
+import com.choo.blog.domain.comments.dto.CommentRequestData;
 import com.choo.blog.domain.posts.Post;
 import com.choo.blog.domain.posts.dto.PostRequestData;
 import com.choo.blog.domain.posts.service.PostService;
@@ -13,16 +15,20 @@ import com.choo.blog.domain.users.User;
 import com.choo.blog.domain.users.UserRole;
 import com.choo.blog.domain.users.dto.UserRegistData;
 import com.choo.blog.domain.users.service.UserService;
+import com.choo.blog.exceptions.PostNotFoundException;
 import com.choo.blog.security.UserAuthentication;
 import com.choo.blog.util.WebTokenUtil;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.test.context.ActiveProfiles;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -41,6 +47,8 @@ class CommentServiceTest {
     @Autowired
     UserProperties userProperties;
     @Autowired
+    CommentService commentService;
+    @Autowired
     private WebTokenUtil webTokenUtil;
 
     private User user;
@@ -58,15 +66,33 @@ class CommentServiceTest {
     @Nested
     @DisplayName("댓글 저장은")
     class Describe_save{
+        CommentRequestData saveData;
+
         @BeforeEach
         void setUp(){
-
+            saveData = prepareRequestData( false, "");
         }
+
         @Nested
         @DisplayName("게시물 id와 댓글이 주어지면")
         class Context_with_postId_and_comments{
+            @Test
+            @DisplayName("댓글을 생성하고 생성된 댓글을 반환한다.")
             void it_save_comments_and_return_commenst(){
+                Comments comments = commentService.save(post.getId(), saveData);
+                assertThat(comments.getContent()).isEqualTo(saveData.getContent());
+                assertThat(comments.isSecret()).isEqualTo(saveData.isSecret());
+            }
+        }
 
+        @Nested
+        @DisplayName("존재하지 않는 게시물 id가 주어지면")
+        class Context_with_invalid_postId{
+            @Test
+            @DisplayName("게시물을 찾을 수 없다는 예외를 던진다")
+            void it_throw_postNotFoundException(){
+                assertThatThrownBy(() -> commentService.save(-1L, saveData))
+                        .isInstanceOf(PostNotFoundException.class);
             }
         }
     }
@@ -88,6 +114,13 @@ class CommentServiceTest {
     private Post preparePost(String suffix){
         PostRequestData postRequestData = postProperties.generateRequestData(category, "");
         return postService.save(postRequestData);
+    }
+
+    private CommentRequestData prepareRequestData(boolean secret, String suffix){
+        return CommentRequestData.builder()
+                .content(CONTENT + suffix)
+                .secret(secret)
+                .build();
     }
 
 }
