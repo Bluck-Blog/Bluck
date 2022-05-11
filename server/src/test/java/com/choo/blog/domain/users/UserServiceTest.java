0b942c41c7fbec85a5ffaeb14abc810510733cfc
ddgg9511@naver.com <ddgg9511@naver.com>
2022-05-11 22:36:42 +0900

    test : user 조회 테스트 코드

--- server/src/test/java/com/choo/blog/domain/users/UserServiceTest.java
+++ server/src/test/java/com/choo/blog/domain/users/UserServiceTest.java
@@ -3,6 +3,7 @@ package com.choo.blog.domain.users;
 import com.choo.blog.common.UserProperties;
 import com.choo.blog.domain.users.dto.UserRegistData;
 import com.choo.blog.domain.users.service.UserService;
+import com.choo.blog.exceptions.UserNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -15,6 +16,7 @@ import org.springframework.test.context.ActiveProfiles;
 import java.time.LocalDate;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @SpringBootTest
 @ActiveProfiles("test")
@@ -51,6 +53,39 @@ class UserServiceTest {
                 assertThat(user.getDescription()).isEqualTo(registData.getDescription());
                 assertThat(user.getNickname()).isEqualTo(registData.getNickname());
                 assertThat(passwordEncoder.matches(registData.getPassword(), user.getPassword())).isTrue();
+            }
+        }
+
+        @Nested
+        @DisplayName("회원 조회는")
+        class Describe_get{
+            @Nested
+            @DisplayName("userId를 입력받으면")
+            class Context_with_userId{
+                User user;
+
+                @BeforeEach
+                void setUp(){
+                    user = userService.join(userProperties.prepareUserRegistData(""));
+                }
+
+                @Test
+                @DisplayName("id에 해당하는 user를 반환한다")
+                void it_return_user(){
+                    User findUser = userService.getUser(this.user.getId());
+
+                    assertThat(findUser).isNotNull();
+                    assertThat(findUser.getId()).isNotZero();
+                }
+            }
+
+            @Nested
+            @DisplayName("존재하지 않는 userId를 입력받으면")
+            class Context_with_non_exist_userId{
+                void it_throw_userNotFoundException(){
+                    assertThatThrownBy(() -> userService.getUser(-1L))
+                            .isInstanceOf(UserNotFoundException.class);
+                }
             }
         }
     }
