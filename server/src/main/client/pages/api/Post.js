502f47e250c0be356a9768059724d549a8293cf2
inaJung <dlsdk0601@gmail.com>
2022-05-23 20:57:33 +0900

    useMutation update and verify email

---
+++ server/src/main/client/pages/api/Post.js
@@ -0,0 +1,19 @@
+import { useMutation } from "react-query";
+import { root } from "../_app";
+import { Axios } from "./Axios";
+
+export const post = async (path, body) => {
+  const { data } = await Axios.post(path, body);
+  return data;
+};
+
+export const POST = {
+  useConfirmEmail: (path) => useMutation((data) => post(path, data)),
+  mutateCallBack: (key) => ({
+    onSuccess: () => {
+      const keyArr = [key];
+      return root.invalidateQueries(keyArr);
+    },
+    onError: (err) => console.log(err),
+  }),
+};
