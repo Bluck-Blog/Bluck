6b1e0dcd7b26dde2dec6d9eb0cb7d1f5016fb9f9
dddgg9511 <ddgg9511@naver.com>
2022-06-15 21:18:00 +0900

    feat : properties 암호화 작업

--- server/src/main/java/com/choo/blog/BlogApplication.java
+++ server/src/main/java/com/choo/blog/BlogApplication.java
@@ -1,7 +1,9 @@
 package com.choo.blog;
 
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.config.Configuration;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
